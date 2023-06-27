7c94396202397ef34d097e9708fbf88eb749202d
shifu-kobby <samuelarmahappiah@gmail.com>
2023-06-27 15:46:23 +0000

    conect newsletter endpoint to db

--- src/lib/rest/endpoints/newsletters/namespace.py
+++ src/lib/rest/endpoints/newsletters/namespace.py
@@ -4,4 +4,5 @@ from src.lib.rest.endpoints.newsletters.newsletters import Newsletters
 
 
 api = Namespace('newsletters', description='', path='/api/newsletters/')
-api.add_resource(Newsletters, '/', methods=['GET', 'POST', 'DELETE'])
+api.add_resource(Newsletters, '/subscribers/', methods=['GET'])
+api.add_resource(Newsletters, '/subscriber/<email>', methods=['POST', 'DELETE'])
--- src/lib/rest/endpoints/newsletters/newsletters.py
+++ src/lib/rest/endpoints/newsletters/newsletters.py
@@ -1,8 +1,36 @@
 from flask_restx import Resource
 
+from src.lib.db.db_utils import connect_to_db
+
+
 class Newsletters(Resource):
     def get(self):
-        return 'works'
+        result = []
+        conn = connect_to_db()
+        cursor = conn.cursor()
+        cmd = 'SELECT * FROM newsletters'
+        cursor.execute(cmd)
+        conn.commit()
 
-    def post(self):
-        return 'works as well'
+        for row in cursor.fetchall():
+            (id, email) = row
+            result.append({
+                'id': id,
+                'email': email
+            })
+
+        cursor.close()
+        conn.close()
+        return result
+
+    def post(self, email):
+        conn = connect_to_db()
+        cursor = conn.cursor()
+        cmd = f"INSERT INTO newsletters(email) VALUES ('{email}')"
+        cursor.execute(cmd)
+        conn.commit()
+
+        cursor.close()
+        conn.close()
+
+        return 'success'
