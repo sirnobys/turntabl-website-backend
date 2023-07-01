7d7d6278efa64324afb1615fd8b49408ef75569e
shifu-kobby <samuelarmahappiah@gmail.com>
2023-07-01 12:19:30 +0000

    added env file to store db credentials

--- src/lib/db/db_utils.py
+++ src/lib/db/db_utils.py
@@ -1,5 +1,16 @@
+import os
+from dotenv import load_dotenv
 from src.lib.db.database import Database
 
 
 def connect_to_db():
-    return Database('localhost', 'postgres', 'postgres', '1234', 5432)
+    load_dotenv()
+    hostname = os.getenv('DB_HOSTNAME')
+    db_name = os.getenv('DB_NAME')
+    user_name = os.getenv('DB_USER_NAME')
+    password = os.getenv('DB_PASSWORD')
+    port = os.getenv('DB_PORT')
+    print(hostname, db_name, user_name, password, port)
+    if not (hostname and db_name and user_name and password and port):
+        return None
+    return Database(hostname, db_name, user_name, password, port)
